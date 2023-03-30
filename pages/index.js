64fe94b3f0e0fe9956ff49788ec22937ca162718
jvdionizio <dionizio145@gmail.com>
2023-03-29 21:55:12 -0300

    feat: connect to bluetooth device

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
+import { connectService } from "@/services/bluetoothService"
+
 export default function Home() {
+
+  const connect = () => {
+    connectService()
+  }
+
   return (
     <>
       <div className='text-red-500 font-bold'>Hello tailwindcss</div>
+      <button
+        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+        onClick={connect}
+      >
+        Connect
+      </button>
     </>
   )
 }
