0fa255393316efbf1f11dec2cc497a89aa824d1f
jvdionizio <dionizio145@gmail.com>
2023-03-30 08:06:27 -0300

    fix: adapting bluetooth service to works with a react component

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { connectService, disconnectService, logToTerminal } from "@/services/bluetoothService"
+import BluetoothTerminal from "@/services/bluetoothService/BluetoothTerminal"
 import { useEffect, useState } from "react"
 
 export default function Home() {
@@ -6,9 +6,9 @@ export default function Home() {
   const [deviceLogs, setDeviceLogs] = useState('')
 
   useEffect(() => {
-    console.log('entrou');
-    setDeviceLogs(logToTerminal)
-  }, [logToTerminal])
+    console.log(deviceLogs)
+  }, [deviceLogs])
+
 
   const connect = () => {
     connectService()
@@ -17,6 +17,53 @@ export default function Home() {
   const disconnect = () => {
     disconnectService()
   }
+
+/* // Helpers.
+const defaultDeviceName = 'Terminal';
+const terminalAutoScrollingLimit = terminalContainer.offsetHeight / 2;
+let isTerminalAutoScrolling = true;
+
+const scrollElement = (element) => {
+  const scrollTop = element.scrollHeight - element.offsetHeight;
+
+  if (scrollTop > 0) {
+    element.scrollTop = scrollTop;
+  }
+}; */
+
+  // Obtain configured instance.
+  const terminal = new BluetoothTerminal();
+
+
+  // Override default log method to output messages to the terminal and console.
+  terminal._log = function(...messages) {
+    // We can't use `super._log()` here.
+    messages.forEach((message) => {
+      console.log("message", message);
+      setDeviceLogs(message);
+    });
+  };
+
+  // Implement own send function to log outcoming data to the terminal.
+  const send = (data) => {
+    terminal.send(data).
+        then(() => setDeviceLogs(data)).
+        catch((error) => setDeviceLogs(data));
+  };
+
+
+  // Bind event listeners to the UI elements.
+  const connectService = () => {
+  terminal.connect().
+      then(() => {
+        deviceNameLabel.textContent = terminal.getDeviceName() ?
+            terminal.getDeviceName() : defaultDeviceName;
+      });
+  };
+
+  const disconnectService = () => {
+  terminal.disconnect();
+  };
 
   return (
     <>
@@ -39,7 +86,6 @@ export default function Home() {
       </div>
       <div className='border border-gray-300 rounded-md p-2'>
           <p>Log</p>
-          <p>{ deviceLogs }</p>
       </div>
     </>
   )
