dc692130e43fc7ab6c8e352542c0211163eb9739
jvdionizio <dionizio145@gmail.com>
2023-04-19 09:28:33 -0300

    Fix: bluetooth service

--- pages/bluetooth.js
+++ pages/bluetooth.js
@@ -202,7 +202,7 @@ export default function BluetoothPage() {
       <div>
         <button
           className='bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
-          onClick={() => send('oi caralho')}
+          onClick={() => send('olá')}
         >
           Send
         </button>
